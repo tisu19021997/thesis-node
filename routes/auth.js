05cb76b07751bcda004ddf1dacb4d40d87daae0e
tisu19021997 <quangphamm1920@gmail.com>
2020-06-22 18:18:51 +0700

    Route specific for authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const passport = require('passport');
-const controller = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/admin', passport.authenticate('jwt-admin', { session: false }),
+router.get('/admin',
+  passport.authenticate('jwt-admin', { session: false }),
   (req, res) => res.status(200)
-    .send('Authorized'));
+    .json({
+      authenticated: true,
+    }));
 
 module.exports = router;
